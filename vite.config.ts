3431b0f2982c45c5fa34d5805e23bb75ee7e7c25
windyeasy <windyeasy13gb@163.com>
2024-07-20 17:43:12 +0800

    feat: 添加自动化部署

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/generate-api-issue-doc',
   plugins: [
     vue(),
     UnoCSS(),
